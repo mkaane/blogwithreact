a47cf13ffce94c5dc183283737e9e274f5ad08f8
mkaane <45371284+mkaane@users.noreply.github.com>
2022-04-27 18:42:21 +0300

    Icons are created

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,7 +3,25 @@ import "./topbar.css"
 
 export default function TopBar() {
   return (
-    <div className="top">topbar
+    <div className="top">
+        <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem">Home</li>
+                <li className="topListItem">About</li>
+                <li className="topListItem">Contact</li>
+                <li className="topListItem">Write</li>
+                <li className="topListItem">Logout</li>
+            </ul>
+        </div>
+        <div className="topRight">
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
     </div>
   )
 }
