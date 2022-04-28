b2fb634b1f3df4660b601ed2f364c68d8e831b9f
mkaane <45371284+mkaane@users.noreply.github.com>
2022-04-28 18:43:50 +0300

    Header img is added, Sidebar and Posts are created

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,4 +1,5 @@
 import "./header.css"
+
 export default function Header() {
   return (
     <div className="header">
@@ -6,6 +7,9 @@ export default function Header() {
           <span className="headerTitleSm">React & Node</span>
           <span className="headerTitleLg">Blog</span>
         </div>
+        <img className="headerImg" src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" 
+        alt="" 
+        />
     </div>
   )
 }
