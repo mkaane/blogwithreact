15f7f054e7fe34bc437b04b6297d50015a545bb3
mkaane <mkaan97@gmail.com>
2022-05-01 16:28:29 +0300

    Routers are created by using react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Write from "./pages/write/Write"
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <><TopBar /><Register /></>
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} /> //if there is a user, go home page, else go login page
+        <Route path="/register" element={user ? <Home /> : <Register />} /> //if there is a user, go home page, else go register page
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
