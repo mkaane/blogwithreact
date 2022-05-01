5a689047a0f3a0294c5d395b47b26ee793226fd6
mkaane <45371284+mkaane@users.noreply.github.com>
2022-05-01 12:27:22 +0300

    Login page is created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write"
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <><TopBar /><Settings /></>
+    <><TopBar /><Login /></>
   );
 }
 
