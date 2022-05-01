87fb6cb5f8cbebcff655365a78ac14dcc8247f14
mkaane <mkaan97@gmail.com>
2022-05-01 13:06:50 +0300

    Register page is created, React router dom is defined.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -6,8 +6,12 @@ export default function Register() {
     <div className='register'>
         <span className="registerTitle">Login</span>
         <form className='registerForm'>
+            <label>Username</label>
+            <input type="text" className="registerInput" 
+            placeholder='Enter your username' />
             <label>Email</label>
-            <input type="text" className="registerInput" placeholder='Enter your email' />
+            <input type="text" className="registerInput" 
+            placeholder='Enter your email' />
             <label>Password</label>
             <input type="password" className='registerInput' placeholder='Enter your password' />
             <button className='registerButton'>Register</button>
