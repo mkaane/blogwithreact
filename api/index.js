f0b096d8840c36d1cad495b86f226cfff46af82d
mkaane <mkaan97@gmail.com>
2022-05-02 12:35:23 +0300

    Database connection is done

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express()
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
-app.listen("5000", ()=>{
+mongoose.connect(process.env.MONGO_URL, {
+}).then(console.log("connected to mongo")).catch((err) => console.log(err));
+
+//console.log("hello")
+
+app.use("/", (req, res) => {
+    console.log("This is main url") //when we do a request to localhost:5001, it will write This is main url (only localhost:5001/)
+}) //if we write app.use("/kaan")blabla  then when we go to localhost:5001/kaan, this message will show.
+app.listen("5001", ()=>{
     console.log("backend is running");
 });
+
+//why we use nodemon?: normally console.log("hello") wont show, because system does not listen our program, only listens 5001 port.
+//but when we use nodemon, system can listen our program.
