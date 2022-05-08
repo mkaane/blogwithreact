a9558a29c0b4c0f06c1d99a5c8e32488baac74fb
mkaane <mkaan97@gmail.com>
2022-05-08 13:12:37 +0300

    posts, categories are linked

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import "./post.css"
-export default function Post() {
+import {Link} from "react-router-dom"
+export default function Post({post}) {
   return (
     <div className='post'>
-        <img className='postImg' src="https://images.pexels.com/photos/947384/pexels-photo-947384.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-    
+      {post.photo && (
+        <img className='postImg' 
+        src={post.photo} 
+        alt="" />
+      )}
     <div className="postInfo">
         <div className="postCats">
-            <span className="postCat">Music</span>
-            <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className='postCat'>{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-            Lorem ipsum dolor sit amet
-        </span>
+        <Link className='link' to={`/post/${post._id}`}><span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
-        <p className='postDesc'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+        <p className='postDesc'>{post.desc}</p>
     </div>
     </div>
   )
