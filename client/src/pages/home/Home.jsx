89f5c40269d9fec76d4763a2d0ecd3602e36df9b
mkaane <mkaan97@gmail.com>
2022-05-08 12:39:26 +0300

    "axios" is installed, useState, useEffect and fetch are written

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,26 @@
-import "./home.css"
-import React from 'react'
-import Header from "../../components/header/Header"
-import Posts from "../../components/posts/Posts"
-import Sidebar from "../../components/sidebar/Sidebar"
+import "./home.css";
+import React, { useEffect, useState } from 'react';
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts")
+      setPosts(res.data)
+    }
+    fetchPosts()
+  },[])
+  
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts}/>
         <Sidebar />
-        
       </div>
     </>
   )
